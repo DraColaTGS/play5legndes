d49e8c6cbf237d13f42f0400d5fcd8c6cc5f89aa
DraColaTGS <50123084+DraColaTGS@users.noreply.github.com>
2019-05-02 13:16:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("5help \ 5play",{type: 'WATCHING'})
+   client.user.setActivity("5help | 5play",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
